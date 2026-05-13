b22bb2a98440d58bf308beb848d9f47f72afd7db
zaidimftsr <zarmufthisor642@gmail.com>
2026-05-13 10:28:21 +0800

    chore: remove dist from git tracking

--- dist/types/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// ============================================
-// Shared TypeScript interfaces
-// Updated: WorkoutLog punya field baru
-// ============================================
-Object.defineProperty(exports, "__esModule", { value: true });
